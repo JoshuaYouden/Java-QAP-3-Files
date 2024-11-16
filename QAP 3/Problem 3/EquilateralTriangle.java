c3408a96937d5cc158f51ddfec8bdaf3ec3c55a8
Joshua Youden <Joshua.Youden@keyin.com>
2024-11-16 13:46:59 -0330

    Finished code in EquilateralTriangle

--- QAP 3/Problem 3/EquilateralTriangle.java
+++ QAP 3/Problem 3/EquilateralTriangle.java
@@ -1,11 +1,79 @@
-public class EquilateralTriangle {
+public class EquilateralTriangle extends Shape {
     // Attributes
+    private double sideA;
+    private double sideB;
+    private double sideC;
 
     // Constructor
+    public EquilateralTriangle() {
+        if (sideA <= 0 || sideB <= 0 || sideC <= 0) {
+            System.out.println("Error: All sides must be greater than 0.");
+            System.exit(1);
+        }
+
+        if (sideA != sideB && sideB != sideC && sideC != sideA) {
+            System.out.println("Error: The sides are not equal.");
+            System.exit(1);
+        }
+
+        sideA = 1.0;
+        sideB = 1.0;
+        sideC = 1.0;
+    }
+
+    public EquilateralTriangle(double sideA, double sideB, double sideC) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+    public EquilateralTriangle(String name, String color, boolean filled, double sideA, double sideB, double sideC) {
+        super(name, color, filled);
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
 
     // Getters
+    public double getSideA() {
+        return sideA;
+    }
+
+    public double getSideB() {
+        return sideB;
+    }
+
+    public double getSideC() {
+        return sideC;
+    }
 
     // Setters
+    public void setSideA(double sideA) {
+        this.sideA = sideA;
+    }
+
+    public void setSideB(double sideB) {
+        this.sideB = sideB;
+    }
+
+    public void setSideC(double sideC) {
+        this.sideC = sideC;
+    }
 
     // Methods
+    @Override
+    public double getPerimeter() {
+        return sideA + sideB + sideC;
+    }
+
+    @Override
+    public double getArea() {
+        double s = (sideA + sideB + sideC) / 2;
+        return Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+    }
+
+    @Override
+    public String toString() {
+        return "An Equilateral Triangle with sides " + sideA + ", " + sideB + ", and " + sideC + ", which is a subclass of " + super.toString();
+    }
 }
