e937a5ba4799d6df34b925c83b52005520031112
Joshua Youden <Joshua.Youden@keyin.com>
2024-11-16 12:30:53 -0330

    Finished Shape and circle classes

--- QAP 3/Problem 3/Circle.java
+++ QAP 3/Problem 3/Circle.java
@@ -1,11 +1,20 @@
-public class Circle {
+public class Circle extends Shape {
     // Attributes
+    private double radius = 1.0;
 
     // Constructor
-
-    // Getters
-
-    // Setters
+    public Circle(double radius) {
+        this.radius = radius;
+    }
 
     // Methods
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+    
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
 }
--- QAP 3/Problem 3/Shape.java
+++ QAP 3/Problem 3/Shape.java
@@ -1,12 +1,53 @@
 public abstract class Shape {
     // Attributes
     private String name;
+    private String color;
+    private boolean filled;
 
     // Constructor
+    public Shape() {
+        color = "blue";
+        filled = true;
+    }
+
+    public Shape(String name, String color, boolean filled) {
+        this.name = name;
+        this.color = color;
+        this.filled = filled;
+    }
 
     // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
 
     // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
 
     // Methods
+    public abstract double getArea();
+    
+    public abstract double getPerimeter();
+
+    public String toString() {
+        return "A Shape with color of " + color + " and " + (filled ? "filled" : "not filled");
+    }
 }
